427c48642e9dd79eb85738520e69e57068816d75
damian <emmydollar98@gmail.com>
2022-06-24 14:45:46 +0100

    fixed header theme

--- src/component/landing/Header/index.tsx
+++ src/component/landing/Header/index.tsx
@@ -8,32 +8,41 @@ import image2 from "./image/mar_3.jpg";
 import image3 from "./image/mar_4.jpg";
 
 const Header = () => {
-
   const fadeImages = [
     {
       url: image2,
-      caption: "First Slide",
     },
     {
       url: image1,
-      caption: "Second Slide",
     },
     {
       url: image3,
-      caption: "Third Slide",
     },
   ];
   return (
-    <div className="site-page-header-ghost-wrapper" >
+    <div className="site-page-header-ghost-wrapper">
       <PageHeader
+        className={"rif-mx-6 rif-br-2"}
         ghost={false}
         backIcon={false}
         title="RIFERA"
-        subTitle={<Input placeholder="Search product..." />}
+        subTitle={
+          <Input
+            placeholder="Search product..."
+            className={"rif-input"}
+            style={{ paddingLeft: 20 }}
+            size={"large"}
+          />
+        }
         extra={[
-          <Button key="3">About</Button>,
-          <Button key="2">Contact</Button>,
-          <Button key="1" type="primary">
+          <Button key="3" className={"rif-btn"}>
+            About
+          </Button>,
+          <Button key="2" className={"rif-btn"}>
+            {" "}
+            Contact
+          </Button>,
+          <Button key="1" type={"primary"}>
             Help
           </Button>,
         ]}
@@ -45,7 +54,6 @@ const Header = () => {
               <div className="image-container rif-px-6 rif-pt-6 rif-mt-8">
                 <img src={fadeImage.url} className={"rif-slide-image"} />
               </div>
-              <h2>{fadeImage.caption}</h2>
             </div>
           ))}
         </Fade>
