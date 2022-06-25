6034cb679d5863c04c231676c3c0116eaa4a5ede
damian <emmydollar98@gmail.com>
2022-06-25 20:17:32 +0100

    fixed image carousal

--- src/component/landing/Products/index.tsx
+++ src/component/landing/Products/index.tsx
@@ -2,10 +2,9 @@
 
 import React, { useEffect, useState, useRef } from "react";
 import { Spin, Card, Button, Modal } from "antd";
-import { Carousel } from "3d-react-carousal";
 import location from "./location-icon.png";
 import { StoreType } from "../../_shared/types";
-
+import Coverflow from "react-coverflow";
 type PropType = {
   data: Array<Record<string, any>>;
   logo: string;
@@ -42,76 +41,172 @@ const Products = (props: PropType) => {
       </>
     );
   }
+
   return (
-    <div className="home">
+    // @ts-ignore
+    <Coverflow
+      width={960}
+      height={480}
+      displayQuantityOfSide={2}
+      navigation={false}
+      enableHeading={false}
+    >
       {data?.map((store, key) => {
         return (
-          <div className={"rif-flex rif-justify-evenly"} key={key}>
-            <Card hoverable cover={<img alt="example" src={logo} />}>
-              <h2>{store?.name}</h2>
-              <p className="card-description">{store?.description}</p>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="card-btn"
-                onClick={() => {
-                  // showModal(store?.no);
-                  console.log("::::::::::", indivData);
-                }}
-              >
-                View More
-              </Button>
-              <h1 className="card-no"> {store?.no} </h1>
-            </Card>
-            {/*<Modal*/}
-            {/*  width="700px"*/}
-            {/*  visible={isModalVisible}*/}
-            {/*  onOk={handleOk}*/}
-            {/*  onCancel={handleCancel}*/}
-            {/*  className="modal-card"*/}
-            {/*>*/}
-            {/*  <img alt="example" src={logo} />*/}
-            {/*  <h2>{indivData[0]?.name}</h2>*/}
-            {/*  <p className="modal-card-description">*/}
-            {/*    {indivData[0]?.description}*/}
-            {/*  </p>*/}
-            {/*  <div className="modal-div">*/}
-            {/*    <h1 className="modal-card-likes"> {indivData[0]?.likes}</h1>*/}
-            {/*    <h1 className="modal-card-no"> {indivData[0]?.no}</h1>*/}
-            {/*    <h1 className="modal-card-votes"> {indivData[0]?.votes}</h1>*/}
-            {/*  </div>*/}
-            {/*  <h1 className="featured-product">Featured Products</h1>*/}
-            {/*  <hr*/}
-            {/*    style={{*/}
-            {/*      width: "70%",*/}
-            {/*      margin: "auto",*/}
-            {/*      backgroundColor: "#48edb5",*/}
-            {/*      height: "0.5em",*/}
-            {/*    }}*/}
-            {/*  />*/}
-            {/*  <div ref={divRef} className="products-img" onClick={() => {}}>*/}
-            {/*    <img alt="example" src={logo} />*/}
-            {/*    <img alt="example" src={logo} />*/}
-            {/*    <img alt="example" src={logo} />*/}
-            {/*    <img alt="example" src={logo} />*/}
-            {/*    <img alt="example" src={logo} />*/}
-            {/*    <img alt="example" src={logo} />*/}
-            {/*  </div>*/}
-            {/*  <span onClick={() => divRef.current.scrollBy(-30, 0)}>&lt;</span>*/}
-            {/*  <span*/}
-            {/*    id="span-right"*/}
-            {/*    onClick={() => divRef.current.scrollBy(30, 0)}*/}
-            {/*  >*/}
-            {/*    &gt;*/}
-            {/*  </span>*/}
-            {/*  <Link to="/googlemap">*/}
-            {/*    <img src={location} id="map-img" alt="store location" />*/}
-            {/*  </Link>*/}
-            {/*</Modal>*/};
-          </div>
+          <Card hoverable cover={<img alt="example" src={logo} key={key} />}>
+            <h2>{store?.name}</h2>
+            <p className="card-description">{store?.description}</p>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="card-btn"
+              onClick={() => {
+                // showModal(store?.no);
+                console.log("::::::::::", indivData);
+              }}
+            >
+              View More
+            </Button>
+            <h1 className="card-no"> {store?.no} </h1>
+          </Card>
         );
       })}
-    </div>
+    </Coverflow>
   );
 };
 export default Products;
+{
+  /*<Modal*/
+}
+{
+  /*  width="700px"*/
+}
+{
+  /*  visible={isModalVisible}*/
+}
+{
+  /*  onOk={handleOk}*/
+}
+{
+  /*  onCancel={handleCancel}*/
+}
+{
+  /*  className="modal-card"*/
+}
+{
+  /*>*/
+}
+{
+  /*  <img alt="example" src={logo} />*/
+}
+{
+  /*  <h2>{indivData[0]?.name}</h2>*/
+}
+{
+  /*  <p className="modal-card-description">*/
+}
+{
+  /*    {indivData[0]?.description}*/
+}
+{
+  /*  </p>*/
+}
+{
+  /*  <div className="modal-div">*/
+}
+{
+  /*    <h1 className="modal-card-likes"> {indivData[0]?.likes}</h1>*/
+}
+{
+  /*    <h1 className="modal-card-no"> {indivData[0]?.no}</h1>*/
+}
+{
+  /*    <h1 className="modal-card-votes"> {indivData[0]?.votes}</h1>*/
+}
+{
+  /*  </div>*/
+}
+{
+  /*  <h1 className="featured-product">Featured Products</h1>*/
+}
+{
+  /*  <hr*/
+}
+{
+  /*    style={{*/
+}
+{
+  /*      width: "70%",*/
+}
+{
+  /*      margin: "auto",*/
+}
+{
+  /*      backgroundColor: "#48edb5",*/
+}
+{
+  /*      height: "0.5em",*/
+}
+{
+  /*    }}*/
+}
+{
+  /*  />*/
+}
+{
+  /*  <div ref={divRef} className="products-img" onClick={() => {}}>*/
+}
+{
+  /*    <img alt="example" src={logo} />*/
+}
+{
+  /*    <img alt="example" src={logo} />*/
+}
+{
+  /*    <img alt="example" src={logo} />*/
+}
+{
+  /*    <img alt="example" src={logo} />*/
+}
+{
+  /*    <img alt="example" src={logo} />*/
+}
+{
+  /*    <img alt="example" src={logo} />*/
+}
+{
+  /*  </div>*/
+}
+{
+  /*  <span onClick={() => divRef.current.scrollBy(-30, 0)}>&lt;</span>*/
+}
+{
+  /*  <span*/
+}
+{
+  /*    id="span-right"*/
+}
+{
+  /*    onClick={() => divRef.current.scrollBy(30, 0)}*/
+}
+{
+  /*  >*/
+}
+{
+  /*    &gt;*/
+}
+{
+  /*  </span>*/
+}
+{
+  /*  <Link to="/googlemap">*/
+}
+{
+  /*    <img src={location} id="map-img" alt="store location" />*/
+}
+{
+  /*  </Link>*/
+}
+{
+  /*</Modal>*/
+}
